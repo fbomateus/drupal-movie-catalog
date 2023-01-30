7ae2d69cd53f49d89f25efea9e612b2bae23d12f
Fábio Mateus <fabiomateus.jobs@gmail.com>
2023-01-30 02:42:38 -0300

    feat: custom movie page

--- web/themes/custom/druflix/js/global.js
+++ web/themes/custom/druflix/js/global.js
@@ -13,4 +13,14 @@
     }
   };
 
+  Drupal.behaviors.actorSlider = {
+    attach: function (context, settings) {
+      $('.actor-character .items').not('.slick-initialized').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+      });
+    }
+  };
+
 })(Drupal);
